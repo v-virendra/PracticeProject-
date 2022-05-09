7bfa8180273a852050a3963e1c6a7efa27a1db9d
v-virendra <v.virendra115@gmail.com>
2022-05-09 16:22:42 +0530

    first commit

---
+++ src/main/java/KTCTC1/PractLast/App.java
@@ -0,0 +1,13 @@
+package KTCTC1.PractLast;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
